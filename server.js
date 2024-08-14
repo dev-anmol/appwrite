e4267f7bad9caa295d3c1caa1f862a8468c652f2
Anmol Thakur <anmolll.thakurrr@gmail.com>
2024-08-14 17:40:11 +0530

    updated

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+const PORT = 8000;
+
+const express = require('express')
+const app = express();
+const uuid4 = require('uuid4')
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json())
+
 const sdk = require('node-appwrite')
 
 
@@ -7,27 +17,54 @@ const client = new sdk.Client()
     .setKey('a8bdd6e51f2eeb51efbe08c63a5835b4acc987df0d04bb04b67f370313b660c721099959b275082b64f629e5137b4ee57631fc49c702da505a9134675d9c3158b7cd3b3e3cca005e153248a94e2b3dc0e4c013ddd7185d9c3d0e8d6d8496040463b1a7e394a1f27449755b780b3639b0aa1c3ac9bef233f3ecef64873d3ff3b1') // Your secret API key
 
 const messaging = new sdk.Messaging(client);
+const users = new sdk.Users(client);
 
-const sendEmail = async () => {
+app.post('/register', async (req, res) => {
+    try {
+        const user = req.body;
+        const id = uuid4()
+
+        const result = await users.create(
+            id, //userId
+            user.email, //email (optional)
+            user.tel //telephone (optional)
+        )
+
+        console.log(result);
+
+        if (result) {
+            sendEmail(result)
+            sendSMS(result)
+        }
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+const sendEmail = async (result) => {
+    const id = uuid4();
+    const email = result.email;
+    const userId = result['$id']
     const message = await messaging.createEmail(
-        '2345234a-3432-45f-324324gf43243', //messageId
-        'Welcome!', //subject
-        'Hi', //content
+        id,
+        `welcome ${email}`, //subject
+        'Thank you so much for signing up! We welcome you to our email community', //content
         [], //topics (optional)
-        ['66bc37b6003428746edb'] //users (optional)
+        [userId] //users (optional)
     );
-
-    console.log(message);
+    console.log(message)
 }
 
-const sendSMS = async () => {
+const sendSMS = async (result) => {
+    const id = uuid4();
+    const userId = result['$id']
     const message = await messaging.createSms(
-        '2345234a-3432-45f-687', //message Id
-        'This is the test', //content
+        id,
+        'Thank you so much for signing up! We welcome you to our SMS community', //content
         [], //topics
-        ['66bc37b6003428746edb']
+        [userId]
     )
     console.log(message)
 }
 
-sendSMS();
+app.listen(PORT)
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,85 @@
+import googlePlayLogo from "./images/google-play-store.png"
+import appStoreLogo from "./images/apple-app-store.png"
+import { useState } from "react"
+
 const App = () => {
+
+  const [user, setUser] = useState(null);
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      const response = await fetch("http://localhost:8000/register", options);
+      const data = await response.json();
+      console.log(data);
+
+    } catch (err) {
+      console.error(err)
+
+    }
+
+
+  }
+  const updateUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+
+  }
+  console.log(user);
 
   return (
     <>
-      <div className="App">
+      <div className="auth-modal">
+        <div className="close-icon">✖</div>
+        <h2>Sign Up</h2>
+        <p>
+          By signing up you agree to our terms and conditions. Learn about our Pirvacy Policy before clicking submit.
+        </p>
+        <form onSubmit={registerUser}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email"
+            onChange={updateUser}
+            value={user?.email}
+          />
+          <input
+            type="tel"
+            id="tel"
+            name="tel"
+            placeholder="Phone Number"
+            onChange={updateUser}
+            value={user?.tel}
+          />
+          <label>
+            <input type="checkbox" required />
+            I agree to the terms and conditions.
+          </label>
+          <input
+            type="submit"
+            className="form-button"
+          />
+        </form>
 
+        <hr />
+
+        <h2>download the app</h2>
+        <div className="link-container">
+          <a href="https://play.google.com/store">
+            <img src={googlePlayLogo} alt="google play icon" className="googlePlay" />
+          </a>
+          <a href="https://apple.com/store">
+            <img src={appStoreLogo} alt="apple store icon" className="appStore" />
+          </a>
+
+        </div>
       </div>
     </>
   )
